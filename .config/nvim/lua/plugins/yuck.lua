0746157ea2d3fd1f76020fdd70a44614ed6d2466
7enan <renanplimas24r@gmail.com>
2025-08-21 10:36:24 -0300

    Configuração básica do nvim
    Essa é uma configuração básica do nvim, utilizando o lazyvim

---
+++ .config/nvim/lua/plugins/yuck.lua
@@ -0,0 +1,6 @@
+-- ~/Dotfiles/.config/nvim/lua/plugins/yuck.lua
+return {
+  -- Instala o plugin oficial para a linguagem Yuck, criado pelo desenvolvedor do Eww.
+  -- Este plugin fornece realce de sintaxe e indentação.
+  "elkowar/yuck.vim",
+}
