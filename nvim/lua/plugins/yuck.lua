75c90d4752622daff9523261fd5951a3ce9bd89d
Renan <renanplimas24r@gmail.com>
2025-10-02 17:41:05 -0300

    Remoção do nvim

--- nvim/lua/plugins/yuck.lua
+++
@@ -1,6 +0,0 @@
--- ~/Dotfiles/.config/nvim/lua/plugins/yuck.lua
-return {
-  -- Instala o plugin oficial para a linguagem Yuck, criado pelo desenvolvedor do Eww.
-  -- Este plugin fornece realce de sintaxe e indentação.
-  "elkowar/yuck.vim",
-}
